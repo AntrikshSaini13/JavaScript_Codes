c31662df2f6d3e3335fbfb82ae6b9222c895850e
antriksh <antriksh.saini@hachiweb.com>
2023-07-18 23:02:00 +0530

    converstion

---
+++ JavaScript_codes_Hetish_Sir/day_6.js
@@ -0,0 +1,32 @@
+//  converstion of data type
+
+// let score = true
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+/*
+"33" => 33
+"33ads" => NaN
+true =>1 / false => 0
+*/
+
+// let isloggedIn = "Aman"
+
+// let boolenIsLoggedIn = Boolean(isloggedIn)
+// console.log(boolenIsLoggedIn)
+
+/*
+1 => true; 0=. false
+"" => false
+"Antriksh" => true
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof(stringNumber))
