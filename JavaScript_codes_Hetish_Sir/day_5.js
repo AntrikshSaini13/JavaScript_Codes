6f46cb18c26032223d228a6d13966cfe2c596f9f
antriksh <antriksh.saini@hachiweb.com>
2023-07-18 22:26:43 +0530

    Data type

---
+++ JavaScript_codes_Hetish_Sir/day_5.js
@@ -0,0 +1,25 @@
+// Datatype And ECMA 
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3 ) // we are using node.js not browser 
+
+// console.log(3+3); console.log("HEtish") //code readablity is not good 
+
+let name = "Antriksh"
+let age = 18
+let isloggedIn = false
+let state = null
+
+//number => 2 to power 53
+// bigint 
+// string => "" write double court 
+// boolen => true/false 
+// null => standlone value and this is a object
+// undifine => value is not assigin
+// symbol => unique
+
+// object
+
+console.log(typeof(age))
+console.log(typeof(null)) //typeof is object fir interview
+console.log(typeof(undifine)) // type of is undifine
