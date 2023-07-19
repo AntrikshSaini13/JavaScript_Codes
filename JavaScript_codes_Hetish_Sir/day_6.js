46c8e343b71323fccaa040d5a3d53ad669f74d1c
antriksh <antriksh.saini@hachiweb.com>
2023-07-19 23:09:51 +0530

    converstion

--- JavaScript_codes_Hetish_Sir/day_6.js
+++ JavaScript_codes_Hetish_Sir/day_6.js
@@ -1,12 +1,12 @@
 //  converstion of data type
 
-// let score = true
-// console.log(typeof score)
-// console.log(typeof(score))
+let score = true
+console.log(typeof score)
+console.log(typeof(score))
 
-// let valueInNumber = Number(score)
-// console.log(typeof(valueInNumber))
-// console.log(valueInNumber)
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
 
 
 /*
@@ -15,10 +15,10 @@
 true =>1 / false => 0
 */
 
-// let isloggedIn = "Aman"
+let isloggedIn = "Aman"
 
-// let boolenIsLoggedIn = Boolean(isloggedIn)
-// console.log(boolenIsLoggedIn)
+let boolenIsLoggedIn = Boolean(isloggedIn)
+console.log(boolenIsLoggedIn)
 
 /*
 1 => true; 0=. false
---
+++ JavaScript_codes_Hetish_Sir/day_7.js
@@ -0,0 +1,45 @@
+// ************************ Operations ************************
+let value = 3 
+let negValue = -value
+// console.log(negValue)
+
+//  basic maths Opertaions
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2/2)
+console.log(2%2)
+
+let str1 = "Antriksh"
+let str2 = " Saini"
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2) // here is work on srting 
+console.log(1 + "2") //
+console.log("1" + 2 + 2) // here i salso work om string 
+console.log(1 + 2 + "2") // but here is first additions than add string
+
+console.log((3 + 4) * 5 % 3);
+
+console.log(+true)
+console.log(+"")
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
+
+
+
